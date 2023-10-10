12e1a200666df2544301db5f889ee9d5efb484b5
ignacio <ignacioaalvarezc@gmail.com>
2023-10-10 12:36:56 -0300

    Corrijiendo error escritura

--- src/main/java/com/chi/springecommerce/service/ProductService.java
+++ src/main/java/com/chi/springecommerce/service/ProductService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
-git
+    
     public Product save(Product product);
     public Optional<Product> get(Integer id);
     public void update(Product product);
