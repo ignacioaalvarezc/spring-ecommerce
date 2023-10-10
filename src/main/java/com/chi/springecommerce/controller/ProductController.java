78f501ea4dab15b7d3cd8ae6ef6aceb889e60ef9
ignacio <ignacioaalvarezc@gmail.com>
2023-10-10 13:50:46 -0300

    Creacion de la funcionalidad update

--- src/main/java/com/chi/springecommerce/controller/ProductController.java
+++ src/main/java/com/chi/springecommerce/controller/ProductController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/product")
@@ -37,6 +40,22 @@ public class ProductController {
         User user = new User(1, "", "", "", "", "", "", "", "");
         product.setUser(user);
         productService.save(product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Product product = new Product();
+        Optional<Product> optionalProduct=productService.get(id);
+        product = optionalProduct.get();
+        LOGGER.info("Producto buscado: {}", product);
+        model.addAttribute("product", product);
+        return "product/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product) {
+        productService.update(product);
         return "redirect:/product";
     }
 }
--- src/main/java/com/chi/springecommerce/model/Product.java
+++ src/main/java/com/chi/springecommerce/model/Product.java
@@ -28,7 +28,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
     private String name;
     private String description;
     private String image;
@@ -41,7 +41,7 @@ public class Product {
     @Override
     public String toString() {
         return "Product{" +
-                "id='" + id + '\'' +
+                "id='" + id +
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", image='" + image + '\'' +
