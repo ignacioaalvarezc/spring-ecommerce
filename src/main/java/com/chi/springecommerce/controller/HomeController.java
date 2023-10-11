a1380e0bc50733f5d640c6254b7bf4eda37afac2
ignacio <ignacioaalvarezc@gmail.com>
2023-10-11 10:31:37 -0300

    Funcionalidad que muestra la vista de compra del usuario

--- src/main/java/com/chi/springecommerce/controller/HomeController.java
+++ src/main/java/com/chi/springecommerce/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.chi.springecommerce.controller;
 
 import com.chi.springecommerce.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductService productService;
@@ -18,5 +23,11 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("product", productService.findAll());
         return "user/home";
+    }
+
+    @GetMapping("shop/{id}")
+    public String shop(@PathVariable Integer id) {
+        log.info("Id producto enviado como paramtero {}", id);
+        return "user/shop";
     }
 }
